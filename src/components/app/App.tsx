e64d09262d0bc80c2285f15303f35e7d7a580f6d
user42022 <andrekizilov@yandex.ru>
2023-11-29 23:11:58 +0300

    feat: add pages router

---
+++ src/components/app/App.tsx
@@ -0,0 +1,23 @@
+import { Link, Outlet } from 'react-router-dom';
+const App = () => {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to={'/'}>Main</Link>
+          </li>
+          <li>
+            <Link to={'/uncontrolled'}>Uncontolled Form</Link>
+          </li>
+          <li>
+            <Link to={'/controlled'}>Contolled Form</Link>
+          </li>
+        </ul>
+      </nav>
+      <Outlet />
+    </>
+  );
+};
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import App from './components/app/App';
+import Main from './pages/main/Main';
+import ControlledForm from './pages/controlled/ControlledForm';
+import UncontrolledForm from './pages/uncontrolled/UncontrolledForm';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Main />} />
+          <Route path="/uncontrolled" element={<UncontrolledForm />} />
+          <Route path="/controlled" element={<ControlledForm />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
